00526e7f75ebde597a28a7f32b86ee25bf554a24
DeborahY <deborah.yang8@gmail.com>
2024-04-13 22:32:54 +0200

    Update README.md + Modify Constructor

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DevOps_BiblioJava
+
+Le package dataFrame vise à traiter simplement et efficacement un ensemble de données. Il permet de créer, afficher et 
+manipuler des dataFrames tout comme les dataFrames de la bibliothèque Pandas de Python tout en fournissant des analyses 
+statistiques. 
+
+## Features
+
+
--- src/test/java/TestDataFrame.java
+++ src/test/java/TestDataFrame.java
@@ -11,41 +11,40 @@ import org.junit.Test;
 
 public class TestDataFrame {
 
-    DataFrame<String, String, Integer> voidDF;
-    DataFrame<String, String, Integer> filledDF;
+    DataFrame<String, String, Object> voidDF;
+    DataFrame<String, String, Object> filledDF;
 
+    DataFrame<String, String, Object> dataFrame;
 
     //Initialisation de deux DataFrame : un vide et un rempli
     @Before
     public void init() throws Exception{
         List<String> index1 = List.of();
         List<String> label1 = List.of();
-        List<List<Integer>> values1 = List.of();
-
+        List<List<Object>> values1 = List.of();
         voidDF = new DataFrame<>(index1, label1, values1);
 
 
-        List<String> index2 = List.of("ligne1", "ligne2");
+        List<String> index2 = List.of("ligne1", "ligne2", "ligne3");
         List<String> label2 = List.of("colonne1", "colonne2");
-        List<List<Integer>> values2 = List.of(
-                List.of(1, 2),
-                List.of(3, 4)
+        List<List<Object>> values2 = List.of(
+                List.of(1, 2, 3),
+                List.of("I'm toto", "I'm tata", "I'm titi")
         );
-
         filledDF = new DataFrame<>(index2, label2, values2);
     }
 
 
     /* Tests sur la construction de DataFrame */
 
+
     //Ce test vérifie qu'une exception est levée si le nombre de valeurs dans une colonne n'est pas valide
     @Test (expected = IndexOutOfBoundsException.class)
     public void testCreateDataFrameWithNoValueInColumn() throws Exception {
-        DataFrame<String, String, Integer> dataFrame;
 
         List<String> index2 = List.of("ligne1", "ligne2");
         List<String> label2 = List.of("colonne1", "colonne2", "colonne3");
-        List<List<Integer>> values2 = List.of(
+        List<List<Object>> values2 = List.of(
                 List.of(1, 2),
                 List.of(5, 6)
         );
@@ -56,12 +55,13 @@ public class TestDataFrame {
     //Ce test vérifie qu'une exception est levée si le nombre de valeurs dans une ligne n'est pas valide
     @Test (expected = IndexOutOfBoundsException.class)
     public void testCreateDataFrameWithNoValueInLine() throws Exception {
-        DataFrame<String, String, Integer> dataFrame;
 
         List<String> index2 = List.of("ligne1", "ligne2");
         List<String> label2 = List.of("colonne1", "colonne2", "colonne3");
-        List<List<Integer>> values2 = List.of(
-                List.of(1, 2, 3)
+        List<List<Object>> values2 = List.of(
+                List.of(1, 2),
+                List.of('a', 'b'),
+                List.of("c")
         );
 
         dataFrame = new DataFrame<>(index2, label2, values2);
@@ -70,14 +70,13 @@ public class TestDataFrame {
     //Ce test vérifie qu'une exception est levée si le nombre de lignes est plus grand que le nombre d'indices
     @Test (expected = IndexOutOfBoundsException.class)
     public void testCreateDataFrameWithMoreLineThanRequire() throws Exception {
-        DataFrame<String, String, Integer> dataFrame;
 
         List<String> index2 = List.of("ligne1", "ligne2");
         List<String> label2 = List.of("colonne1", "colonne2", "colonne3");
-        List<List<Integer>> values2 = List.of(
-                List.of(1, 2, 0),
-                List.of(3, 4, 0),
-                List.of(5, 6, 0)
+        List<List<Object>> values2 = List.of(
+                List.of(1, 2),
+                List.of("I'm toto", "I'm tata", "I'm titi"),
+                List.of(5.4, 6.2)
         );
 
         dataFrame = new DataFrame<>(index2, label2, values2);
@@ -86,15 +85,27 @@ public class TestDataFrame {
     //Ce test vérifie qu'une exception est levée si le nombre de colonnes est plus grand que le nombre de labels
     @Test (expected = IndexOutOfBoundsException.class)
     public void testCreateDataFrameWithMoreColumnThanRequire() throws Exception {
-        DataFrame<String, String, Integer> dataFrame;
 
         List<String> index2 = List.of("ligne1", "ligne2");
-        List<String> label2 = List.of("colonne1", "colonne2", "colonne3");
-        List<List<Integer>> values2 = List.of(
-                List.of(1, 2 , 0, 1),
-                List.of(3, 4, 0 , 1)
+        List<String> label2 = List.of("colonne1", "colonne2");
+        List<List<Object>> values2 = List.of(
+                List.of(1, 2),
+                List.of(-3, -4),
+                List.of("I'm toto", "I'm tata")
         );
 
+        dataFrame = new DataFrame<>(index2, label2, values2);
+    }
+
+    /* This test verify that an exception is thrown when a column has elements of different types */
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreateDataFrameWithDifferentTypesInColumn() throws Exception {
+        List<String> index2 = List.of("ligne1", "ligne2");
+        List<String> label2 = List.of("colonne1", "colonne2");
+        List<List<Object>> values2 = List.of(
+                List.of(1, 2),
+                List.of("a", 'a')
+        );
         dataFrame = new DataFrame<>(index2, label2, values2);
     }
 
@@ -102,14 +113,12 @@ public class TestDataFrame {
     // et que le premier est écrasé par le second
     @Test
     public void testCreateDataFrameWithSameIndex() throws Exception {
-        DataFrame<String, String, Integer> dataFrame;
 
-        List<String> index2 = List.of("ligne1", "ligne1", "ligne2");
+        List<String> index2 = List.of("ligne1", "ligne2", "ligne3", "ligne1");
         List<String> label2 = List.of("colonne1", "colonne2");
-        List<List<Integer>> values2 = List.of(
-                List.of(0, 0),
-                List.of(1, 2),
-                List.of(3, 4)
+        List<List<Object>> values2 = List.of(
+                List.of(9, 2, 3, 1),
+                List.of("I'm truc", "I'm tata", "I'm titi", "I'm toto")
         );
 
         dataFrame = new DataFrame<>(index2, label2, values2);
@@ -121,15 +130,14 @@ public class TestDataFrame {
     // et que le premier est écrasé par le second
     @Test
     public void testCreateDataFrameWithSameLabel() throws Exception {
-        DataFrame<String, String, Integer> dataFrame;
 
-        List<String> index2 = List.of("ligne1", "ligne2");
+        List<String> index2 = List.of("ligne1", "ligne2", "ligne3");
         List<String> label2 = List.of("colonne1", "colonne2", "colonne2");
-        List<List<Integer>> values2 = List.of(
-                List.of(1, 0, 2),
-                List.of(3, 0, 4)
+        List<List<Object>> values2 = List.of(
+                List.of(1, 2, 3),
+                List.of(1.2, 3.4, 5.6),
+                List.of("I'm toto", "I'm tata", "I'm titi")
         );
-
         dataFrame = new DataFrame<>(index2, label2, values2);
 
         assertEquals(filledDF.toString(), dataFrame.toString());
